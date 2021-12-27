e921d133231e10ec0c1dfc0ef13026be171b796e
Fabio Jose <fabiojpl.1907@gmail.com>
2021-12-27 13:10:30 -0300

    Mapas - Final

---
+++ src/main/java/fj/mapa/Livro.java
@@ -0,0 +1,43 @@
+package fj.mapa;
+
+import java.util.Objects;
+
+public class Livro {
+
+        private String nome;
+        private Integer paginas;
+
+        public Livro(String nome, Integer paginas) {
+            this.nome = nome;
+            this.paginas = paginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public Integer getPaginas() {
+            return paginas;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            fj.mapa.Livro livro = (fj.mapa.Livro) o;
+            return nome.equals(livro.nome) && paginas.equals(livro.paginas);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nome, paginas);
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", paginas=" + paginas +
+                    '}';
+        }
+}
---
+++ src/main/java/fj/mapa/Ordenar.java
@@ -0,0 +1,66 @@
+package fj.mapa;
+
+import java.util.*;
+
+public class Ordenar {
+    public static void main(String[] args) {
+
+
+        System.out.println("\n-----------------------");
+        System.out.println("--\tOrdem aleatória - Nome Autor + Livro\t--");
+        // usar hashmap
+        Map<String, Livro> meusLivros = new HashMap<>() {{
+            put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
+            put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
+            put(" Harari, Yuval Noah", new Livro("21 Lições Para o Século 21", 432));
+        }};
+        for (Map.Entry<String, Livro> livro : meusLivros.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+
+
+        System.out.println("\n-----------------------");
+        System.out.println("--\tOrdem Inserção Nome Autor + Livro\t--");
+        // usar LinkedHashMap<>
+        Map<String, Livro> meusLivros1 = new LinkedHashMap<>() {{
+            put(" Hawking, Stephen", new Livro("Uma Breve História do Tempo", 256));
+            put(" Duhigg, Charles", new Livro("O Poder do Hábito", 408));
+            put(" Harari, Yuval Noah", new Livro("21 Lições Para o Século 21", 432));
+        }};
+        for (Map.Entry<String, Livro> livro : meusLivros1.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+
+
+        System.out.println("\n-----------------------");
+        System.out.println("--\tOrdem alfabética autores\t--");
+        // ordem natural TreeMap
+        Map<String, Livro> meusLivros2 = new TreeMap<>(meusLivros1);
+        for (Map.Entry<String, Livro> livro : meusLivros2.entrySet())
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+
+
+        System.out.println("\n-----------------------");
+        System.out.println("--\tOrdem alfabética nomes dos livros\t--");
+        // nome do livro é valor
+        // precisa usar treeset
+        // precisa ter comparator
+        Set<Map.Entry<String, Livro>> meusLivros3 = new TreeSet<>(new ComparatorNome());
+        meusLivros3.addAll(meusLivros.entrySet());
+        for (Map.Entry<String, Livro> livro : meusLivros3)
+            System.out.println(livro.getKey() + " - " + livro.getValue().getNome());
+
+
+    }
+}
+
+class ComparatorNome implements Comparator<Map.Entry<String, Livro>>{
+
+    @Override
+    public int compare(Map.Entry<String, Livro> l1, Map.Entry<String, Livro> l2) {
+        return l1.getValue().getNome().compareToIgnoreCase(l2.getValue().getNome());
+    }
+
+
+} // fim ordenar
